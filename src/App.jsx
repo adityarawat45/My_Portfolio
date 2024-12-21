4c1c03f9445818667351d13b4c739457c64f19d0
Aditya <rawataditya193@gmail.com>
2024-12-21 21:16:41 +0530

    Hided bottom bar from live app

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <div className="h-screen w-screen inset-0 bg-gradient-to-t from-slate-900 to-sky-950"></div>
     </div>
     <div className="hidden md:block lg:block mb-32"><Nav></Nav></div>
-    <div className="block md:hidden lg:hidden"><Bottombar></Bottombar></div>
+    {/* <div className="block md:hidden lg:hidden"><Bottombar></Bottombar></div> */}
     <Hero></Hero>
      <About></About>
     <Work></Work>
