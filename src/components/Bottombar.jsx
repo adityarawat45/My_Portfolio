06c02770517c0cefeda670e818abc6d96fa73147
Aditya <rawataditya193@gmail.com>
2024-12-21 21:14:05 +0530

    Setted up the position of bottom bar

--- src/components/Bottombar.jsx
+++ src/components/Bottombar.jsx
@@ -1,11 +1,9 @@
 const Bottombar = () => {
   return (
-    <div className="flex-col w-full h-screen fixed z-10">
-      <div className="px-5">
-        <nav className="bg-black rounded-xl">
+    <div className="flex flex-col justify-end w-full h-screen fixed z-10">
+        <nav className="bg-black rounded-lg px-5 mx-5 mb-3 py-2">
             Heyyy
         </nav>
-    </div>
     </div>
   )
 }
