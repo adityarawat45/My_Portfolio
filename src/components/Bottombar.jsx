9a5d856275c6eda31b57d94e11eb9a71e8fad93d
Aditya Rawat <rawataditya193@gmail.com>
2025-02-08 21:41:41 +0530

    First commit

--- src/components/Bottombar.jsx
+++ src/components/Bottombar.jsx
@@ -2,7 +2,7 @@ const Bottombar = () => {
   return (
     <div className="flex flex-col justify-end w-full h-screen fixed z-10">
         <nav className="bg-black rounded-lg px-5 mx-5 mb-3 py-2">
-            Heyyy
+            Heyyy, gonna create a bottom_bar later for the mobile view
         </nav>
     </div>
   )
