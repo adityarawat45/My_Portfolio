3bfa5f4cf609e2b8e7417c17849119d7b1e28fdb
adityarawat45 <rawataditya193@gmail.com>
2025-03-14 21:29:36 +0530

    Added bottom bar, and icons

--- src/components/Bottombar.jsx
+++ src/components/Bottombar.jsx
@@ -1,8 +1,19 @@
+import { FaHome } from "react-icons/fa";
+import { MdWork, MdLocalPostOffice} from "react-icons/md";
+
 const Bottombar = () => {
   return (
     <div className="flex flex-col justify-end w-full h-screen fixed z-10">
-        <nav className="bg-black rounded-lg px-5 mx-5 mb-3 py-2">
-            Heyyy, gonna create a bottom_bar later for the mobile view
+        <nav className="border-2 border-slate-700 flex flex-row items-center justify-between bg-slate-950 backdrop:blur-md bg-opacity-30 rounded-lg px-5 mx-5 mb-3 py-2">
+            <div>
+              <FaHome></FaHome>
+            </div>
+            <div>
+              <MdWork></MdWork>
+            </div>
+            <div>
+            <MdLocalPostOffice></MdLocalPostOffice>
+            </div>
         </nav>
     </div>
   )
