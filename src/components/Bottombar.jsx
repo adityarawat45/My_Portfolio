d4415c12fee7909b8ef43a4df0f065e415278248
adityarawat45 <rawataditya193@gmail.com>
2025-04-30 16:38:32 +0530

    animation and size change

--- src/components/Bottombar.jsx
+++ src/components/Bottombar.jsx
@@ -42,19 +42,37 @@ const Bottombar = () => {
     <motion.div
       initial={{ y: 100 }}
       animate={{ y: showBar ? 0 : 100 }}
-      transition={{ type: "spring", stiffness: 300, damping: 30 }}
+      transition={{ type: "spring", stiffness: 500, damping: 50 }}
       className="fixed bottom-0 w-full z-50"
     >
-      <nav className="border-2 border-slate-700 flex flex-row items-center justify-between bg-slate-950 backdrop-blur-md bg-opacity-30 rounded-lg px-5 mx-5 mb-3 py-2 pointer-events-auto">
-        <button onClick={() => scrollToSection("home")} aria-label="Home" className="text-white">
-          <FaHome className="text-xl" />
+      <nav className="border-2 border-slate-700 flex flex-row items-center justify-between bg-slate-950 backdrop-blur-md bg-opacity-30 rounded-3xl px-5 mx-5 mb-3 pb-3 pt-5 pointer-events-auto">
+        <motion.div
+          className="inline-block relative mx-3 md:mx-0"
+          initial={{ scale: 1.1 }}
+          whileHover={{ scale: 0.9}} 
+          transition={{ type: "stiffness", stiffness: 200 }}>
+                  <button onClick={() => scrollToSection("home")} aria-label="Home" className="text-white">
+                      <FaHome className="text-xl" />
         </button>
-        <button onClick={() => scrollToSection("about")} aria-label="About" className="text-white">
+        </motion.div>
+        <motion.div
+          className="inline-block relative mx-3 md:mx-0"
+          initial={{ scale: 1.1 }}
+          whileHover={{ scale: 0.9}} 
+          transition={{ type: "stiffness", stiffness: 200 }}>
+                    <button onClick={() => scrollToSection("about")} aria-label="About" className="text-white">
           <MdWork className="text-xl" />
         </button>
+        </motion.div>
+        <motion.div
+          className="inline-block relative mx-3 md:mx-0"
+          initial={{ scale: 1.1 }}
+          whileHover={{ scale: 0.9}} 
+          transition={{ type: "stiffness", stiffness: 200 }}>
         <button onClick={() => scrollToSection("work")} aria-label="Work" className="text-white">
           <MdLocalPostOffice className="text-xl" />
         </button>
+        </motion.div>
       </nav>
     </motion.div>
   );
