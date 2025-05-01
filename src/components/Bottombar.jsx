b189f44263e17f5a6efce5b758abee46a9fdfa62
Aditya Rawat <rawataditya193@gmail.com>
2025-05-01 19:16:12 +0530

    changes

--- src/components/Bottombar.jsx
+++ src/components/Bottombar.jsx
@@ -36,15 +36,15 @@ const Bottombar = () => {
       transition={{ type: "spring", stiffness: 100, bounce: 0.5, duration: 0.1 }}
       className="fixed bottom-0 w-full z-50"
     >
-      <nav className="border-2 border-slate-700 flex flex-row items-center justify-between bg-slate-950 backdrop-blur-md bg-opacity-10 rounded-2xl px-8 mx-5 mb-3 py-3 pointer-events-auto">
-        <button onClick={() => scrollToSection("home")} aria-label="Home" className="text-white cursor-pointer">
-          <FaHome className="text-xl" />
+      <nav className="border-2 border-slate-700 flex flex-row items-center justify-between bg-slate-950 backdrop-blur-md bg-opacity-10 rounded-2xl px-8 mx-5 mb-3 py-4 pointer-events-auto">
+        <button onClick={() => scrollToSection("home")} aria-label="Home" className="text-slate-200 cursor-pointer">
+          <FaHome className="text-xl size-7" />
         </button>
-        <button onClick={() => scrollToSection("about")} aria-label="About" className="text-white cursor-pointer">
-          <MdWork className="text-xl" />
+        <button onClick={() => scrollToSection("about")} aria-label="About" className="text-slate-200 cursor-pointer">
+          <MdWork className="text-xl size-7" />
         </button>
-        <button onClick={() => scrollToSection("work")} aria-label="Work" className="text-white cursor-pointer">
-          <MdLocalPostOffice className="text-xl" />
+        <button onClick={() => scrollToSection("work")} aria-label="Work" className="text-slate-200 cursor-pointer">
+          <MdLocalPostOffice className="text-xl size-7" />
         </button>
       </nav>
     </motion.div>
