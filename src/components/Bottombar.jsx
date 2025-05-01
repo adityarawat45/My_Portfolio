8569916c1c27e5fd9e5b6762c3b237f24f2552b3
adityarawat45 <rawataditya193@gmail.com>
2025-05-01 11:07:11 +0530

    changes

--- src/components/Bottombar.jsx
+++ src/components/Bottombar.jsx
@@ -6,29 +6,22 @@ import { motion } from "framer-motion";
 const Bottombar = () => {
   const [showBar, setShowBar] = useState(true);
   const lastScrollY = useRef(0);
-  const timeoutRef = useRef(null);
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
-
-      if (currentScrollY > lastScrollY.current) {
-        setShowBar(false);
+  
+      if (currentScrollY < lastScrollY.current) {
+        setShowBar(true); // scrolling up
       } else {
-        setShowBar(true);
+        setShowBar(false); // scrolling down or same
       }
-
+  
       lastScrollY.current = currentScrollY;
-
-      clearTimeout(timeoutRef.current);
-      timeoutRef.current = setTimeout(() => {
-        setShowBar(true);
-      }, 150); 
     };
-
+  
     window.addEventListener("scroll", handleScroll);
     return () => {
-      clearTimeout(timeoutRef.current);
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
@@ -42,17 +35,17 @@ const Bottombar = () => {
     <motion.div
       initial={{ y: 100 }}
       animate={{ y: showBar ? 0 : 100 }}
-      transition={{ type: "spring", stiffness: 300, damping: 30 }}
+      transition={{ type: "spring", stiffness: 100, bounce : 0.5, duration : 0.1}}
       className="fixed bottom-0 w-full z-50"
     >
-      <nav className="border-2 border-slate-700 flex flex-row items-center justify-between bg-slate-950 backdrop-blur-md bg-opacity-30 rounded-lg px-5 mx-5 mb-3 py-2 pointer-events-auto">
-        <button onClick={() => scrollToSection("home")} aria-label="Home" className="text-white">
+      <nav className="border-2 border-slate-700 flex flex-row items-center justify-between bg-slate-950 backdrop-blur-md bg-opacity-10 rounded-2clxl px-8 mx-5 mb-3 py-3 pointer-events-auto">
+        <button onClick={() => scrollToSection("home")} aria-label="Home" className="text-white cursor-pointer">
           <FaHome className="text-xl" />
         </button>
-        <button onClick={() => scrollToSection("about")} aria-label="About" className="text-white">
+        <button onClick={() => scrollToSection("about")} aria-label="About" className="text-white cursor-pointer">
           <MdWork className="text-xl" />
         </button>
-        <button onClick={() => scrollToSection("work")} aria-label="Work" className="text-white">
+        <button onClick={() => scrollToSection("work")} aria-label="Work" className="text-white cursor-pointer">
           <MdLocalPostOffice className="text-xl" />
         </button>
       </nav>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -7,7 +7,11 @@ const Work = () => {
         <div className='flex-col justify-center px-4 md:px-6'>
         <div className="text-4xl md:text-6xl font-tiny5 md:mt-24 md:mb-8 mt-8 text-center font-bold text-slate-200">Work & Exp.</div>
         {/* Technosoft */}
-        {<WorkTemplate profile={"Backend Developer Intern"} org={"AX Technosoft"} Location={"Gurugram"} mode={"On-Site"} start={"Sept, 2024"} end={"Current"}></WorkTemplate>}
+        {<WorkTemplate profile={"Backend Developer Intern"} org={"AX Technosoft"} Location={"Gurugram"} mode={"On-Site"} start={"Sept, 2024"} end={"Current"}
+        pointOne={"Integrated many CRM and ERP apps like Odoo, Quickbooks and Twilio. Including connectors with auth, rest apis and many other customization from scratch."}
+        pointTwo={"Implemented the complete functionality of deleting an entity with an improved time complexity of 0(n) with the use of a hashmap."}
+        pointThree={"Worked on different app and flow router apis, and contributed by resolving various bugs"}
+        ></WorkTemplate>}
         {/* Techlive */}
         {<WorkTemplate profile={"MERN Stack Trainee"} org={"Techlive"} mode={"Remote"} Location={"Remote"} start={"June, 2024"} end={"Aug, 2024"} pointOne={"Worked on a variety of web development projects using the MERN stack."} pointTwo={"Learned about building dynamic and responsive web applications. "} pointThree={"Gained practical experience and sharpened coding skills through hands-on projects and exercises."}></WorkTemplate>}
         </div>
